d7ff1f24d92fc3ea1e519def3b990b7f333c64df
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-11-21 04:50:43 -0300

    adicionado dependencia yaml e adicionado documentação swagger customizado.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import yaml
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 from routes.volunteer import router as volunteer_router
 
 app = FastAPI(title="Sistema de Gerenciamento de Voluntários")
 
 app.include_router(volunteer_router)
+
+
+with open("openapi.yaml", "r") as f:
+    custom_openapi_yaml = yaml.safe_load(f)
+
+
+def custom_openapi():
+    return custom_openapi_yaml
+
+
+app.openapi = custom_openapi
